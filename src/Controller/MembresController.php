1996ac1e973e4833dcc80afd1a1f6b273f45069a
Alejandro Gabriel y Galan <alexgyg@msn.com>
2018-08-10 16:34:32 +0200

    dev: controller - membres

--- src/Controller/MembresController.php
+++ src/Controller/MembresController.php
@@ -18,21 +18,24 @@ class MembresController extends Controller
 
     public function list(MembresRepository $membresRepository): Response
     {
-        return $this->render('tests/test.html.twig', 
+        return $this->render('tests/list.html.twig', 
             ['membres' => $membresRepository->findAll()]);
     }
 
     public function show(Membres $pseudo): Response
     {
-        return $this->render('tests/test.html.twig', ['pseudo' => $pseudo]);
+        // Apparemment, en passant un id en argument sf4 s'arrange 
+        // pour que show() reçoive un objet membre complet.
+        return $this->render('tests/show.html.twig', ['pseudo' => $pseudo]);
     }
 
     public function new(Request $request): Response
     {
         // Instanciation de la classe Membres
+        $message="" ;
         $membre = new Membres();
 
-        // Dédinition du formulaire
+        // Définition du formulaire
         $form = $this->createFormBuilder($membre)
             ->add('pseudo')
             ->add('password')
@@ -40,38 +43,56 @@ class MembresController extends Controller
             ->add('save', SubmitType::class)
             ->getForm();
 
-        // Traitement de la soumission du formulaire
-        $form->handleRequest($request);
+            $repository = $this->getDoctrine()->getRepository(Membres::class);
+            $tempmemberemail = $repository->findOneBy(["email" => $request->get('email')]);
 
-        // Si tout se passe bien, enregistrement du membre dans la base
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($membre);
-            $em->flush();
+        if(is_null($tempmemberemail)){
+            // Traitement de la soumission du formulaire
+            $form->handleRequest($request);
 
-            // Après l'enregistrement, affichage de la liste de membres
-            return $this->redirectToRoute('test_list');
+            // Si tout se passe bien, enregistrement du membre dans la base
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($membre);
+                $em->flush();
+
+                // Après l'enregistrement, affichage de la liste de membres
+                return $this->redirectToRoute('test_list');
+            }
+
+        }else{
+            $message="Il y a un loupé.";
         }
 
+
+
+
         // Affichage du formulaire (qui n'a pas encore été soumis)
-        return $this->render('tests/test.html.twig', [
+        return $this->render('tests/form.html.twig', [
             'membre' => $membre,
             'form' => $form->createView(),
+            'message' => $message
         ]);
     }
 
     public function edit(Request $request, Membres $membre): Response
     {
-        $form = $this->createForm(MembresType::class, $membre);
+        $form = $this->createFormBuilder($membre)
+        ->add('pseudo')
+        ->add('password')
+        ->add('email')
+        ->add('save', SubmitType::class)
+        ->getForm();
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('membres_edit', ['id' => $membre->getId()]);
+            return $this->redirectToRoute('test_list', ['id' => $membre->getId()]);
         }
 
-        return $this->render('membres/edit.html.twig', [
+        return $this->render('tests/form.html.twig', [
             'membre' => $membre,
             'form' => $form->createView(),
         ]);
@@ -79,13 +100,11 @@ class MembresController extends Controller
 
     public function delete(Request $request, Membres $membre): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$membre->getId(), $request->request->get('_token'))) {
             $em = $this->getDoctrine()->getManager();
             $em->remove($membre);
             $em->flush();
-        }
 
-        return $this->redirectToRoute('membres_index');
+        return $this->redirectToRoute('test_list');
     }
 
 
