855ad376d532ddd0e3cac301141d978cc602c64f
Emmanuel ROBACHE <manu337810@gmail.com>
2018-08-24 16:56:18 +0200

    mise a jour messagerie ok 16h56

--- src/Controller/MessagerieController.php
+++ src/Controller/MessagerieController.php
@@ -62,29 +62,26 @@ class MessagerieController extends Controller
     public function showMail(Request $request, \Swift_Mailer $mailer){
         
         $membres = $this->getDoctrine()->getRepository(Membres::class)->findAll();
+        if($request->get("send")){
+            if($request->get("exampleFormControlSelect2") == "all"){ // boucle envoi mail à tous les users
+                for($i =0; $i < count($membres); $i++)
+                    $this->sendMail($membres[$i]->getEmail(), $mailer);
+            }else{
+                $this->sendMail($request->get("exampleFormControlSelect2"), $mailer);
+            }
+        }
+        return $this->render('admin3/send_mailing.html.twig', array("membres" => $membres));
+    }
 
-
+    private function sendMail($adresse, $mailer){
         $message = (new \Swift_Message('Bonjour'))
         ->setFrom('WF3biscuits@gmail.com')
-        ->setTo('WF3biscuits@gmail.com')
+        ->setTo($adresse)
         ->setBody(
-            $this->renderView(
-                'admin3/mail/send.html.twig',
-                array('membres' => $membres)
-            ),
+            $this->renderView('admin3/mail/send.html.twig'),
             'text/html'
         );
         $mailer->send($message);
-        // if($request->get("send")){
-        //     if($request->get("exampleFormControlSelect2") == "all"){
-        //         for($i =0; $i < count($membres); $i++)
-           
-        //     }else{
-                    
-        //         $request->get("exampleFormControlSelect2");
-        //     }
-        // }
-        return $this->render('admin3/send_mailing.html.twig', array("membres" => $membres));
     }
 
  
